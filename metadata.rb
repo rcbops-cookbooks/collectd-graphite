d6f4eb43112769ad10a4c61dfa9e8da469c0276c
rackerjoe <breu@breu.org>
2013-10-11 09:45:13 -0500

    move version out of metadata.rb into VERSION

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "osops@lists.launchpad.net"
 license          "Apache 2.0"
 description      "glues collectd and graphite cookbooks together"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.1.3"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 %W{ centos ubuntu }.each do |distro|
   supports distro
